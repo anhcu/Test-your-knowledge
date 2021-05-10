5a9f13c497bb752ea8ee8b0b29d1676fa096e1e5
Hunter <no@no.com>
2021-05-10 16:39:51 -0400

    need to work on function

---
+++ quiz-question.js
@@ -0,0 +1,12 @@
+let quiz = [
+    {
+        question:  "What is HTML stand for?",
+        option: [
+            "Hyper Tag Markup Language",
+            "High Type Mark Language",
+            "Hyper Text Markup Language",
+            "Hyperlink Text Marking Language",
+        ],
+        answer: 2,
+    }
+]
